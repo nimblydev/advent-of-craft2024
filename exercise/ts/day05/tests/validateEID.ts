f4c6e3fea9d39d1dac0fe6c684739afe2ed34c30
Yoann Aubry <yoann@nimbly.dev>
2024-12-06 00:45:56 +0100

    refacto: extract sex digit validation into a separate function for improved readability

--- exercise/ts/day05/tests/validateEID.ts
+++ exercise/ts/day05/tests/validateEID.ts
@@ -3,6 +3,11 @@ export const validateEID = (eid: string) => {
   if (eid.length !== 8) return false;
 
   const sexDigit = eid.at(0);
-  if (!["1", "2", "3"].includes(sexDigit)) return false;
+  if (!sexDigitValidation(sexDigit)) return false;
+
   return true;
+};
+
+const sexDigitValidation = (sexDigit: string) => {
+  return ["1", "2", "3"].includes(sexDigit);
 };
