b6d8ec0d3902697c2a4a89225454948f77c2e607
Yoann Aubry <yoann@nimbly.dev>
2024-12-06 00:38:30 +0100

    refacto: simplify EID validation for empty string check

--- exercise/ts/day05/tests/validateEID.ts
+++ exercise/ts/day05/tests/validateEID.ts
@@ -1,5 +1,5 @@
 export const validateEID = (eid: string) => {
-  if (eid === null || eid === "") return false;
+  if (!eid) return false;
   if (eid.length < 8) return false;
   if (!["1", "2", "3"].includes(eid.at(0))) return false;
   return true;
