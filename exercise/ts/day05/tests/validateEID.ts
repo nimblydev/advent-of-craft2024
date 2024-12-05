0cb3b5d2d86296a1d047ae6e68b76acae2949e12
Yoann Aubry <yoann@nimbly.dev>
2024-12-06 00:43:27 +0100

    refacto: extract sex digit from EID for improved readability in validation

--- exercise/ts/day05/tests/validateEID.ts
+++ exercise/ts/day05/tests/validateEID.ts
@@ -1,6 +1,8 @@
 export const validateEID = (eid: string) => {
   if (!eid) return false;
   if (eid.length !== 8) return false;
-  if (!["1", "2", "3"].includes(eid.at(0))) return false;
+
+  const sexDigit = eid.at(0);
+  if (!["1", "2", "3"].includes(sexDigit)) return false;
   return true;
 };
