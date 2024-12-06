514b23a5f8b831d112f1de60b4d57be34bfac9a1
Yoann Aubry <yoann@nimbly.dev>
2024-12-06 14:56:01 +0100

    refactor: improve control key validation by simplifying logic and enhancing readability

--- exercise/ts/day05/src/validateEID.ts
+++ exercise/ts/day05/src/validateEID.ts
@@ -1,9 +1,5 @@
-const EID_LENGTH = 8;
-const ELVEN_SEX = ["1", "2", "3"];
 const validIEDPattern = /^[123]\d{7}$/;
 const MODULUS = 97;
-const CONTROL_KEY_LENGTH = 2;
-const ZERO_PADDING_CHAR = "0";
 
 type Validator = (eid: string, eidPart?: string) => boolean;
 type Range = { start: number; end?: number };
@@ -14,13 +10,15 @@ export const validateEID = (potentialEID: string) =>
 const validateChecksum = (potentialEID: string) =>
   isControlKeyValid(potentialEID);
 
-const isControlKeyValid: Validator = (potentialEID: string) =>
-  calculatedKey(extractPrefix(potentialEID)) === extractKey(potentialEID);
+const isControlKeyValid: Validator = (potentialEID: string) => {
+  const calculateKey = calculatedKey(extractPrefix(potentialEID));
 
+  const extract = extractKey(potentialEID);
+  return extract === calculateKey;
+};
 const extractPrefix = (potentialEID: string) =>
   parseInt(potentialEID.substring(0, 6));
 
-const extractKey = (eid: string) => eid.substring(6, 8);
+const extractKey = (eid: string) => parseInt(eid.substring(6, 8));
 
-const calculatedKey = (prefix: number) =>
-  (MODULUS - (prefix % MODULUS)).toString();
+const calculatedKey = (prefix: number) => MODULUS - (prefix % MODULUS);
