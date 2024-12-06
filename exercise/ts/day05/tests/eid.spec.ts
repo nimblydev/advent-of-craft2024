d0985bd74e1f2b6b810889690bd9f4c2dd2ad119
Yoann Aubry <yoann@nimbly.dev>
2024-12-06 10:56:14 +0100

    refacto: update EID validation tests to use toBeFalsy and toBeTruthy for clarity

--- exercise/ts/day05/tests/eid.spec.ts
+++ exercise/ts/day05/tests/eid.spec.ts
@@ -2,34 +2,34 @@ import { validateEID } from "../src/validateEID";
 
 describe("EID validation", () => {
   it("should return false for null EID", () => {
-    expect(validateEID(null)).toBe(false);
+    expect(validateEID(null)).toBeFalsy();
   });
 
   it("should return false for empty string", () => {
-    expect(validateEID("")).toBe(false);
+    expect(validateEID("")).toBeFalsy();
   });
 
   it("should return true for 19845606 (valid EID)", () => {
-    expect(validateEID("19845606")).toBe(true);
+    expect(validateEID("19845606")).toBeTruthy();
   });
 
   it("should return false for 198456066 a too long EID)", () => {
-    expect(validateEID("198456066")).toBe(false);
+    expect(validateEID("198456066")).toBeFalsy();
   });
 
   it("should return false for 2230 a too short EID)", () => {
-    expect(validateEID("2230")).toBe(false);
+    expect(validateEID("2230")).toBeFalsy();
   });
 
   it("should return false for 40000325 (invalid sex digit)", () => {
-    expect(validateEID("40000325")).toBe(false);
+    expect(validateEID("40000325")).toBeFalsy();
   });
 
   it("should return false for 00000394 (invalid sex digit)", () => {
-    expect(validateEID("00000394")).toBe(false);
+    expect(validateEID("00000394")).toBeFalsy();
   });
 
   it("should return false for 1ab14599 (invalid characters)", () => {
-    expect(validateEID("1ab14599")).toBe(false);
+    expect(validateEID("1ab14599")).toBeFalsy();
   });
 });
