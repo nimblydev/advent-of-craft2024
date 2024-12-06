3445fccb19e0cb57cf49e916b3bdccea9e523481
Yoann Aubry <yoann@nimbly.dev>
2024-12-06 15:56:53 +0100

    code review

--- exercise/ts/day06/src/elfWorkshop.ts
+++ exercise/ts/day06/src/elfWorkshop.ts
@@ -1,16 +1,27 @@
 export class ElfWorkshop {
-    taskList: string[] = [];
+  // readability: the class name should be more explicit, like ElfTaskManager
+  // best-practice: for the sake of immutability, the taskList should be a private property and should be accessed through a getter
+  // best-practice: the taskList should be readonly and initialized in the constructor
+  // best-practice: use underscore to indicate that the property is private
+  taskList: string[] = [];
 
-    addTask(task: string): void {
-        if (task !== "") {
-            this.taskList.push(task);
-        }
-    }
+  // best practice: where is the constructor ? it should be used to initialize the taskList
 
-    completeTask(): string {
-        if (this.taskList.length > 0) {
-            return this.taskList.shift();
-        }
-        return null;
+  // we could avoid a primitive type for task and use a Task class
+  addTask(task: string): void {
+    if (task !== "") {
+      this.taskList.push(task);
     }
+  }
+  // completeTask() returns a string but it can also return null
+  completeTask(): string {
+    // question: no sorting, so the first task added is the first to be removed ?
+    if (this.taskList.length > 0) {
+      // with an other approach, we could return the first uncompleted task, without testing if the taskList is empty
+      // functional: shift() removes the first element, a better approach with a non-primitive Task would be to filter uncompleted tasks and return the first one
+      // then change its status to completed
+      return this.taskList.shift();
+    }
+    return null; // functional/question:  why not return an empty string or an explicit message ?
+  }
 }
--- exercise/ts/day06/tests/elfWorkshop.spec.ts
+++ exercise/ts/day06/tests/elfWorkshop.spec.ts
@@ -1,35 +1,51 @@
-import {ElfWorkshop} from "../src/elfWorkshop";
+import { ElfWorkshop } from "../src/elfWorkshop";
 
-describe('ElfWorkshop Tasks', () => {
-    test('removeTask should add a task', () => {
-        const workshop = new ElfWorkshop();
-        workshop.addTask("Build toy train");
-        expect(workshop.taskList).toContain("Build toy train");
-    });
+describe("ElfWorkshop Tasks", () => {
+  /* use a beforeEach to avoid repeating the same code in each test
+    
+    ```typescript
+    let emptyWorkshop: ElfWorkshop;
+    beforeEach(() => {
+        emptyWorkshop = new ElfWorkshop();
+    }
+    ``
+    */
 
-    test('test2 checks for task addition', () => {
-        const workshop = new ElfWorkshop();
-        workshop.addTask("Craft dollhouse");
-        expect(workshop.taskList.includes("Craft dollhouse")).toBeTruthy();
-    });
+  // typo in the description ? removeTask should be addTask ?
+  test("removeTask should add a task", () => {
+    const workshop = new ElfWorkshop();
+    workshop.addTask("Build toy train");
+    expect(workshop.taskList).toContain("Build toy train");
+  });
+  // why don,'t use fast-check to generate random strings and replace the hardcoded strings  and the 3 first tests ?
+  // test2 is not a good name, it should be more descriptive
+  test("test2 checks for task addition", () => {
+    const workshop = new ElfWorkshop();
+    workshop.addTask("Craft dollhouse");
+    expect(workshop.taskList.includes("Craft dollhouse")).toBeTruthy();
+  });
+  // test2 is not a good name and already used above, it should be more descriptive
+  test("test2 checks for task addition", () => {
+    const workshop = new ElfWorkshop();
+    workshop.addTask("Paint bicycle");
+    expect(workshop.taskList.includes("Paint bicycle")).toBeTruthy();
+  });
+  // readability: if you prefere to use should in the test name, why not use it() instead of test() ?
+  // explicitness: the test name should be more explicit about what behavior is being tested
+  test("Should handle empty tasks correctly", () => {
+    const workshop = new ElfWorkshop();
+    workshop.addTask(""); // readability: replace "" by a constant EMPTY_STRING
+    expect(workshop.taskList.length).toBe(0);
+  });
+  // expliotness: the test name should be more explicit about what behavior is being tested
+  // like "Should remove the older task from the list"
+  test("Task removal functionality", () => {
+    const workshop = new ElfWorkshop();
+    workshop.addTask("Wrap gifts");
+    const removedTask = workshop.completeTask();
+    expect(removedTask).toBe("Wrap gifts");
+    expect(workshop.taskList.length).toBe(0); // best practice: 1 expectation by test
+  });
 
-    test('test2 checks for task addition', () => {
-        const workshop = new ElfWorkshop();
-        workshop.addTask("Paint bicycle");
-        expect(workshop.taskList.includes("Paint bicycle")).toBeTruthy();
-    });
-
-    test('Should handle empty tasks correctly', () => {
-        const workshop = new ElfWorkshop();
-        workshop.addTask("");
-        expect(workshop.taskList.length).toBe(0);
-    });
-
-    test('Task removal functionality', () => {
-        const workshop = new ElfWorkshop();
-        workshop.addTask("Wrap gifts");
-        const removedTask = workshop.completeTask();
-        expect(removedTask).toBe("Wrap gifts");
-        expect(workshop.taskList.length).toBe(0);
-    });
+  // missing coverage: the completeTask() method should be tested when the taskList is empty
 });
