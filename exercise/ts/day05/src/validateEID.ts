a475a0dc2f505a01c017f42c48436d7bc448d4c1
Yoann Aubry <yoann@nimbly.dev>
2024-12-06 14:37:40 +0100

    refactor: simplify EID validation by replacing multiple validators with a regex pattern and checksum validation

--- exercise/ts/day05/src/validateEID.ts
+++ exercise/ts/day05/src/validateEID.ts
@@ -1,6 +1,6 @@
 const EID_LENGTH = 8;
 const ELVEN_SEX = ["1", "2", "3"];
-const numericStringPattern = /^\d+$/;
+const validIEDPattern = /^[123]\d{7}$/;
 const MODULUS = 97;
 const CONTROL_KEY_LENGTH = 2;
 const ZERO_PADDING_CHAR = "0";
@@ -8,40 +8,14 @@ const ZERO_PADDING_CHAR = "0";
 type Validator = (eid: string, eidPart?: string) => boolean;
 type Range = { start: number; end?: number };
 
-export const validateEID = (eid: string) => {
-  if (isEmpty(eid)) return false;
-  const extractEIDPartAndValidate = extractPartIfNecessaryAndValidate(eid);
+export const validateEID = (potentialEID: string) =>
+  validIEDPattern.test(potentialEID) && validateChecksum(potentialEID);
 
-  const validatorDescriptors: [Validator, Range?][] = [
-    [isLengthValid],
-    [isOnlyDigits, { start: 0, end: 8 }],
-    [isValidSexDigit, { start: 0, end: 1 }],
-    [isValidEIDYear, { start: 1, end: 3 }],
-    [isControlKeyValid, { start: 6, end: 8 }],
-  ];
-  return validatorDescriptors.every(([validator, range]) =>
-    extractEIDPartAndValidate(range)(validator)
-  );
-};
-
-const isValidSexDigit: Validator = (eid: string) => {
-  const sexDigit = eid.substring(0, 1);
-  return ELVEN_SEX.includes(sexDigit);
-};
-
-const isEmpty: Validator = (eid: string) => eid === null || eid === "";
-
-const isLengthValid: Validator = (eid: string) => eid.length === EID_LENGTH;
-
-const isOnlyDigits: Validator = (eid: string) => numericStringPattern.test(eid);
-
-const isValidEIDYear: Validator = (eid: string, yearPart?: string) => {
-  return isOnlyDigits(yearPart);
-};
+const validateChecksum = (potentialEID: string) =>
+  isControlKeyValid(potentialEID, potentialEID.substring(6, 8));
 
 const isControlKeyValid: Validator = (eid: string, controlKeyPart?: string) => {
   const eidPrefix = eid.substring(0, 6);
-
   const modulo = parseInt(eidPrefix, 10) % MODULUS;
   const complementedValue = (MODULUS - modulo)
     .toString()
@@ -49,7 +23,3 @@ const isControlKeyValid: Validator = (eid: string, controlKeyPart?: string) => {
 
   return complementedValue === controlKeyPart;
 };
-
-const extractPartIfNecessaryAndValidate =
-  (eid: string) => (range?: Range) => (eidValidatorFn: Validator) =>
-    eidValidatorFn(eid, eid.substring(range?.start ?? 0, range?.end));
