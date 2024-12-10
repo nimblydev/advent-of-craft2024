35178324dadc4c2ef0ceb43117e10f7b2fc70026
Yoann Aubry <yoann@nimbly.dev>
2024-12-10 23:42:20 +0100

    refactor: improve whichFloor method and add helper functions

--- exercise/ts/day10/src/delivery/building.ts
+++ exercise/ts/day10/src/delivery/building.ts
@@ -1,25 +1,29 @@
 export class Building {
-    static whichFloor(instructions: string): number {
-        let val: Array<[string, number]> = [];
+  static whichFloor(instructions: string): number {
+    let val: Array<[string, number]> = [];
 
-        for (let i = 0; i < instructions.length; i++) {
-            const c = instructions[i];
-
-            if (instructions.includes("🧝")) {
-                const j = c === ')' ? 3 : -2;
-                val.push([c, j]);
-            } else if (!instructions.includes("🧝")) {
-                val.push([c, c === '(' ? 1 : -1]);
-            } else {
-                val.push([c, c === '(' ? 42 : -2]);
-            }
-        }
-
-        let result = 0;
-        for (const kp of val) {
-            result += kp[1];
-        }
-
-        return result;
+    for (const element of instructions) {
+      if (instructions.includes("🧝")) {
+        const j = isClosingParenthesis(element) ? 3 : -2;
+        val.push([element, j]);
+      } else if (!instructions.includes("🧝")) {
+        val.push([element, isOpeningParenthesis(element) ? 1 : -1]);
+      } else {
+        val.push([element, isOpeningParenthesis(element) ? 42 : -2]);
+      }
     }
+
+    return Building.calculateTotalFromTuples(val);
+  }
+
+  private static calculateTotalFromTuples(val: [string, number][]) {
+    return val.reduce((acc, [_, j]) => acc + j, 0);
+  }
+}
+function isOpeningParenthesis(c: string) {
+  return c === "(";
+}
+
+function isClosingParenthesis(c: string) {
+  return c === ")";
 }
