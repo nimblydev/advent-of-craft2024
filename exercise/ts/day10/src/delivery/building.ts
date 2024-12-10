8dfd6625efe67c07840782cc4af4c59f9ad053e9
Yoann Aubry <yoann@nimbly.dev>
2024-12-11 00:46:56 +0100

    refactor: enhance whichFloor method by introducing character value calculation functions and simplifying logic

--- exercise/ts/day10/src/delivery/building.ts
+++ exercise/ts/day10/src/delivery/building.ts
@@ -1,26 +1,30 @@
-const sumArray = (values: Array<number>) =>
-  values.reduce((acc, current) => acc + current, 0);
-
+const ELF = "🧝";
 const isOpeningParenthesis = (c: string) => c === "(";
 
 const isClosingParenthesis = (c: string) => c === ")";
 
+const calculateLetterValueWhenElfEmoji = (c: string) => {
+  if (isElf(c)) return 0;
+  return isClosingParenthesis(c) ? 3 : -2;
+};
+
+const calculateLetterValueWhenNoElfEmoji = (c: string) =>
+  isOpeningParenthesis(c) ? 1 : -1;
+
+const isElf = (c: string) => c === ELF;
+const withElf = (letters) => letters.some(isElf);
+
 export class Building {
   static whichFloor(instructions: string): number {
-    let val: Array<number> = [];
+    const characters = [...instructions];
+    const calculationFn = withElf(characters)
+      ? calculateLetterValueWhenElfEmoji
+      : calculateLetterValueWhenNoElfEmoji;
 
-    for (const element of instructions) {
-      let valueToPush: number;
-      if (instructions.includes("🧝")) {
-        valueToPush = isClosingParenthesis(element) ? 3 : -2;
-      } else if (!instructions.includes("🧝")) {
-        valueToPush = isOpeningParenthesis(element) ? 1 : -1;
-      } else {
-        valueToPush = isOpeningParenthesis(element) ? 42 : -2;
-      }
-      val.push(valueToPush);
-    }
-
-    return sumArray(val);
+    return computeCharacterSum(calculationFn)(characters);
   }
 }
+const computeCharacterSum =
+  (calculationFn: Function) =>
+  (characters: string[]): number =>
+    characters.reduce((acc, current) => acc + calculationFn(current), 0);
