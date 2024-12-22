8e31031c80c14e9f807f420ef3636ef927dd205c
Yoann Aubry <yoann@nimbly.dev>
2024-12-22 12:11:11 +0100

    feat: implement reindeer API with middleware for API key and versioning

---
+++ exercise/ts/day20/src/apiKeyMiddleware.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from "express";
+
+export const apiKeyMiddleware =
+  (validApiKey: string) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    const apiKey = req.headers["x-api-key"];
+    if (apiKey && apiKey === validApiKey) {
+      next();
+    } else {
+      res.status(403).send("Forbidden: Invalid API Key");
+    }
+  };
