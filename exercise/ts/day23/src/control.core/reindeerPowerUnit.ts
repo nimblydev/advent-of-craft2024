5b0fef6637063282172d43dc08c4576b05f124d9
Yoann Aubry <yoann@nimbly.dev>
2024-12-23 23:01:10 +0100

    fix: use a better Magic Power Amplifier

--- exercise/ts/day23/src/control.core/reindeerPowerUnit.ts
+++ exercise/ts/day23/src/control.core/reindeerPowerUnit.ts
@@ -1,24 +1,26 @@
-import {Reindeer} from "../external/deer/reindeer";
-import {MagicPowerAmplifier} from "./magicPowerAmplifier";
-import {AmplifierType} from "./amplifierType";
+import { Reindeer } from "../external/deer/reindeer";
+import { MagicPowerAmplifier } from "./magicPowerAmplifier";
+import { AmplifierType } from "./amplifierType";
 
 export class ReindeerPowerUnit {
-    public reindeer: Reindeer;
-    public amplifier: MagicPowerAmplifier = new MagicPowerAmplifier(AmplifierType.BASIC);
+  public reindeer: Reindeer;
+  public amplifier: MagicPowerAmplifier = new MagicPowerAmplifier(
+    AmplifierType.BLESSED
+  );
 
-    constructor(reindeer: Reindeer) {
-        this.reindeer = reindeer;
-    }
+  constructor(reindeer: Reindeer) {
+    this.reindeer = reindeer;
+  }
 
-    public harnessMagicPower(): number {
-        if (!this.reindeer.needsRest()) {
-            this.reindeer.timesHarnessing++;
-            return this.amplifier.amplify(this.reindeer.getMagicPower());
-        }
-        return 0;
+  public harnessMagicPower(): number {
+    if (!this.reindeer.needsRest()) {
+      this.reindeer.timesHarnessing++;
+      return this.amplifier.amplify(this.reindeer.getMagicPower());
     }
+    return 0;
+  }
 
-    public checkMagicPower(): number {
-        return this.reindeer.getMagicPower();
-    }
+  public checkMagicPower(): number {
+    return this.reindeer.getMagicPower();
+  }
 }
