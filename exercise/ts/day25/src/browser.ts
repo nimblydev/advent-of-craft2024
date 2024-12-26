3f06750d77252635061a88ac0b608285145c201a
Yoann Aubry <yoann@nimbly.dev>
2024-12-26 17:43:14 +0100

    feat: update dependencies and add time capsule HTML page

--- exercise/ts/day25/src/browser.ts
+++ exercise/ts/day25/src/browser.ts
@@ -1,29 +1,29 @@
-import { exec } from 'child_process';
-import * as os from 'os';
+import { exec } from "child_process";
+import * as os from "os";
 
 export class Browser {
-    static open(filePath: string): void {
-        const platform = os.platform();
-        let command: string;
+  static open(filePath: string): void {
+    const platform = os.platform();
+    let command: string;
 
-        if (platform === "win32") {
-            // Windows
-            command = `start "" "${filePath}"`;
-        } else if (platform === "darwin") {
-            // macOS
-            command = `open "${filePath}"`;
-        } else if (platform === "linux") {
-            // Linux
-            command = `xdg-open "${filePath}"`;
-        } else {
-            console.error("Unsupported platform for opening a browser.");
-            return;
-        }
-
-        exec(command, (error) => {
-            if (error) {
-                console.error("Error opening browser:", error);
-            }
-        });
+    if (platform === "win32") {
+      // Windows
+      command = `start "" "${filePath}"`;
+    } else if (platform === "darwin") {
+      // macOS
+      command = `open "${filePath}"`;
+    } else if (platform === "linux") {
+      // Linux
+      command = `firefox "${filePath}"`;
+    } else {
+      console.error("Unsupported platform for opening a browser.");
+      return;
     }
+
+    exec(command, (error) => {
+      if (error) {
+        console.error("Error opening browser:", error);
+      }
+    });
+  }
 }
