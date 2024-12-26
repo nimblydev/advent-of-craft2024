f7bafcbab935d2407e8d72066eb03bdc82a8f1d9
Yoann Aubry <yoann@nimbly.dev>
2024-12-26 16:49:54 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 9
        test: 0
    test-stats:
        run: 0
        passed: 0
        failed: 0
        skipped: 0
        error: 0
        duration: 0s

--- exercise/ts/day24/src/domain/core/Event.ts
+++ exercise/ts/day24/src/domain/core/Event.ts
@@ -1,11 +1,14 @@
-import {v4 as uuidv4} from 'uuid';
-import {IEvent} from "./IEvent";
+import { v4 as uuidv4 } from "uuid";
+import { IEvent } from "./IEvent";
 
+/**
+ * @deprecated
+ *
+ */
 export class Event implements IEvent {
-    constructor(
-        public id: string = uuidv4(),
-        public version: number = 1,
-        public date: Date = new Date()
-    ) {
-    }
+  constructor(
+    public id: string = uuidv4(),
+    public version: number = 1,
+    public date: Date = new Date()
+  ) {}
 }
