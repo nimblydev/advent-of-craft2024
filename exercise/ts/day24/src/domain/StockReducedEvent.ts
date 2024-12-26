bcefc3d60c5c02698c0e72c4285032fdb6b5d780
Yoann Aubry <yoann@nimbly.dev>
2024-12-26 16:51:56 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 4
        test: 0
    test-stats:
        run: 0
        passed: 0
        failed: 0
        skipped: 0
        error: 0
        duration: 0s

--- exercise/ts/day24/src/domain/StockReducedEvent.ts
+++ exercise/ts/day24/src/domain/StockReducedEvent.ts
@@ -1,13 +1,13 @@
-import {Event} from "./core/Event"
-import {StockUnit} from "./StockUnit";
+import { Event } from "./core/Event";
+import { StockUnit } from "./StockUnit";
 
 export class StockReducedEvent extends Event {
-    constructor(
-        id: string,
-        date: Date,
-        public toyName: string,
-        public newStock: StockUnit
-    ) {
-        super(id, 1, date);
-    }
+  constructor(
+    id: string,
+    date: Date,
+    public itemName: string,
+    public newStock: StockUnit
+  ) {
+    super(id, 1, date);
+  }
 }
