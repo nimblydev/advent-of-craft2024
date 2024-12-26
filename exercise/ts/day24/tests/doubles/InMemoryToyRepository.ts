37019ad01d262c0de6e0f83759cc5eed1292ea54
Yoann Aubry <yoann@nimbly.dev>
2024-12-26 16:34:33 +0100

    refactor: improve formatting and structure in InMemoryToyRepository class

--- exercise/ts/day24/tests/doubles/InMemoryToyRepository.ts
+++ exercise/ts/day24/tests/doubles/InMemoryToyRepository.ts
@@ -1,39 +1,39 @@
-import {none, Option, some} from 'fp-ts/Option';
-import {IToyRepository} from "../../src/domain/IToyRepository";
-import {Toy} from "../../src/domain/Toy";
+import { none, Option, some } from "fp-ts/Option";
+import { IToyRepository } from "../../src/domain/IToyRepository";
+import { Toy } from "../../src/domain/Toy";
 
-import {IEvent} from "../../src/domain/core/IEvent";
+import { IEvent } from "../../src/domain/core/IEvent";
 
 export class InMemoryToyRepository implements IToyRepository {
-    private _toys: Map<string, Toy> = new Map<string, Toy>();
-    private _raisedEvents: IEvent[] = [];
+  private _toys: Map<string, Toy> = new Map<string, Toy>();
+  private _raisedEvents: IEvent[] = [];
 
-    findByName(name: string): Option<Toy> {
-        for (let toy of this._toys.values()) {
-            if (toy.name === name) {
-                return some(toy);
-            }
-        }
-        return none;
+  findByName(name: string): Option<Toy> {
+    let toyIndexFound = this._toys.has(name) ? name : -1;
+    if (toyIndexFound !== -1) {
+      return some(this._toys.get(name));
     }
 
-    findById(id: string): Option<Toy> {
-        const toy = this._toys.get(id);
-        return toy ? some(toy) : none;
-    }
+    return none;
+  }
 
-    save(toy: Toy): void {
-        this._raisedEvents = [];
-        this._toys = this._toys.set(toy.getId(), toy);
+  findById(id: string): Option<Toy> {
+    const toy = this._toys.get(id);
+    return toy ? some(toy) : none;
+  }
 
-        toy.getUncommittedEvents().forEach((event) => {
-            this._raisedEvents.push(event);
-        });
+  save(toy: Toy): void {
+    this._raisedEvents = [];
+    this._toys = this._toys.set(toy.getId(), toy);
 
-        toy.clearUncommittedEvents();
-    }
+    toy.getUncommittedEvents().forEach((event) => {
+      this._raisedEvents.push(event);
+    });
 
-    raisedEvents(): IEvent[] {
-        return this._raisedEvents;
-    }
+    toy.clearUncommittedEvents();
+  }
+
+  raisedEvents(): IEvent[] {
+    return this._raisedEvents;
+  }
 }
