c5c3a71de21162be800cdecf20c000f38d05617e
Yoann Aubry <yoann@nimbly.dev>
2024-12-26 17:16:17 +0100

    ✅ TCR - tests passing
    changed-lines:
        src: 3
        test: 0
    test-stats:
        run: 0
        passed: 0
        failed: 0
        skipped: 0
        error: 0
        duration: 0s

--- exercise/ts/day24/src/usecases/ToyDeliveryUseCase.ts
+++ exercise/ts/day24/src/usecases/ToyDeliveryUseCase.ts
@@ -42,7 +42,8 @@ export class TDUC {
 
   private errorFor(deliverToy: DeliverToy): DomainError {
     return DomainError.anError(
-      `Oops we have a problem... we have not built the toy: ${deliverToy.desiredToy.toString()}`
+      "Oops we have a problem... we have not built the toy: " +
+        deliverToy.desiredToy.toString()
     );
   }
 }
